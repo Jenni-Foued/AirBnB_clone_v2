e46bd073004619af74a67812594b1c884f2f6199
Jenni-Foued <medfouedjenni@gmail.com>
2021-05-02 08:32:11 +0100

    using format instead of +

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,19 +3,20 @@
 Fabric script that generates a .tgz archive
 from the contents of the web_static folder
 """
+
 from fabric.api import local
 from datetime import datetime
-from os.path import isdir
+from os.path import exists
 
 
 def do_pack():
     """generates a .tgz archive"""
-    try:
-        date = datetime.now().strftime("%Y%m%d%H%M%S")
-        if isdir("versions") is False:
-            local("mkdir verions")
-        file_name = "versions/web_static_{}.tgz".format(date)
-        local("tar -cvzf {} web_static".format(file_name))
-        return file_name
-    except:
-        return None
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    if not (exists('versions')):
+        local('mkdir -p verions')
+    file_name = "versions/web_static_{}.tgz".format(date)
+    local("tar -cvzf {} web_static".format(file_name))
+    return file_name
+    if (exits(file_name)):
+        return path
+    return None
